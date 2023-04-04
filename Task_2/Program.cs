c69475297f9ad384e6fea31efd8da56d3cb8920d
Кирилл <kirilllcit@gmail.com>
2023-04-05 00:50:09 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var res = Sort(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
+
+Console.WriteLine(string.Join(", ", res));
+
+
+static int[] Sort(int[] nums)
+{
+    var result  = new int[nums.Length];
+
+    var zeroNums = nums.Where(_ => _ == 0).ToArray();
+
+    int j = 0;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != 0)
+        {
+            result[j] = nums[i];
+
+            j++;
+        }
+    }
+
+    return result.Concat(zeroNums).ToArray();
+}
