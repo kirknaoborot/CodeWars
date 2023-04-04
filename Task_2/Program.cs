e09d9810f953cd9449cddf2c02c3f036686eafa9
Кирилл <kirilllcit@gmail.com>
2023-04-05 01:01:10 +0300

    правка

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
 
 var res = Sort(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 });
 
@@ -9,19 +9,5 @@ static int[] Sort(int[] nums)
 {
     var result  = new int[nums.Length];
 
-    var zeroNums = nums.Where(_ => _ == 0).ToArray();
-
-    int j = 0;
-
-    for (int i = 0; i < nums.Length; i++)
-    {
-        if (nums[i] != 0)
-        {
-            result[j] = nums[i];
-
-            j++;
-        }
-    }
-
-    return result.Concat(zeroNums).ToArray();
+    return nums.OrderBy(x => x == 0).ToArray();
 }
