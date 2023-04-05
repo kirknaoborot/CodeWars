dff848e1ad111b090bd3b5dae81f4257115afd2d
Кирилл <kirilllcit@gmail.com>
2023-04-05 15:11:35 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿var result = GetReadableTime(60);
+
+Console.WriteLine(result);
+static string GetReadableTime(int inputSeconds)
+{
+    var time = TimeSpan.FromSeconds(inputSeconds);
+
+    var hours = time.TotalHours < 10 ? $"0{Convert.ToInt32(Math.Floor(time.TotalHours))}" : Convert.ToInt32(Math.Floor(time.TotalHours)).ToString();
+
+    var minutes = time.Minutes < 10 ? $"0{time.Minutes}" : time.Minutes.ToString();
+
+    var seconds = time.Seconds < 10 ? $"0{time.Seconds}" : time.Seconds.ToString();
+
+    return $"{hours}:{minutes}:{seconds}";
+}
